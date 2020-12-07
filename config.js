1016adfeb17375500a1e6d61500269d0b78c721e
Malek Haj-Hussein <malekadair@gmail.com>
2020-12-06 21:45:28 -0600

    scrapped some unused stock images. Removed pathPrefix in config

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
+  // pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'Malek Haj-Hussein',
   heading: 'Software Engineer',
   // social
